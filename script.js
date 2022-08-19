d0a5f6ea81b602b1057636c1ca091cc6dd9790d7
Denis <fox.ruden@gmail.com>
2022-08-20 02:37:02 +0300

    save

--- script.js
+++ script.js
@@ -1,65 +1,48 @@
 // пропишем функцию random
+const body = document.querySelector("body");
+const end = document.querySelector(".end");
 const random = (min, max) => {
-    const rand = min + Math.random() * (max - min + 1);
-    return Math.floor(rand);
-}
-
+ const rand = min + Math.random() * (max - min + 1);
+ return Math.floor(rand);
+};
+let total = 0;
 // найдем кнопку
-const btn = document.querySelector('#btn');
+const btn = document.querySelector("#btn");
 // повесим обработчик событий
-btn.addEventListener('mouseenter', () => {
-    btn.style.left = `${random(0, 90)}%`;
-    btn.style.top = `${random(0, 90)}%`;
+btn.addEventListener("mouseenter", () => {
+ if (x > 0) {
+  btn.style.left = `${random(0, 90)}%`;
+  btn.style.top = `${random(0, 90)}%`;
+ }
+
+ if (x === 0) {
+  btn.style.left = `50%`;
+  btn.style.top = `50%`;
+ }
 });
 
 // в случае победы выведем:
-btn.addEventListener('click', () => {
-    alert('Congrats! You hit the button! What are you doing with your life? :D');
+btn.addEventListener("click", () => {
+ alert("Congrats! You hit the button! What are you doing with your life? :D");
+ total += 1;
 });
 
+let timer; // пока пустая переменная
+let x = "30"; // стартовое значение обратного отсчета
+countdown(); // вызов функции
+function countdown() {
+ // функция обратного отсчета
+ let y = x.toString();
+ document.getElementById("rocket").innerHTML = y.padStart(2, "0");
+ x--; // уменьшаем число на единицу
+ if (x < 0) {
+  btn.classList.add("rotate-90-cw");
+  end.classList.remove("end");
 
+  clearTimeout(timer); // таймер остановится на нуле
+ } else {
+  timer = setTimeout(countdown, 1000);
+ }
+}
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-// // функция рандом
-// const random = (min, max) => {
-//   const rand = min + Math.random() * (max - min + 1);
-//   return Math.floor(rand);
-// };
-
-// // убегающая кнопка
-// const btn = document.querySelector('#btn');
-// btn.addEventListener('mouseenter', () => {
-//     btn.style.left = `${random(0, 90)}%`;
-//     btn.style.top = `${random(0, 90)}%`;
-// });
-
-// // победа
-// btn.addEventListener('click',
-//   () => alert('Congratulations! You hit the button! What are you doing with your life?')
-// );
+// rotate-90-cw
